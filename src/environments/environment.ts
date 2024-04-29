5d39fc4e3c508e62bf6be2c5b68ada5a3a42c2f9
Andrew Barker <andrewbarker96@gmail.com>
2024-04-29 12:19:47 -0500

    Environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  supabaseURL: process.env.REACT_APP_SUPABASE_URL,
+  supabaseKey: process.env.REACT_APP_SUPABASE_KEY,
+  supabaseAnonKey: process.env.REACT_APP_SUPABASE_ANON_KEY,
+  sigParserAPIKey: process.env.SIGPARSER_KEY,
+  sigParserAPIURL: process.env.SIGPARSER_API_URL,
+  sigParserSecretKey: process.env.SIGPARSER_SECRET_KEY,
+};
