5fce14aef281ad65637bcc215406a86d4ba46ade
Andrew Barker <andrewbarker96@gmail.com>
2024-05-23 15:17:25 -0500

    ionic build

--- build/assets/ios.transition-legacy-C0psJp-y.js
+++ build/assets/ios.transition-legacy-C0psJp-y.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-FZ1lvydi.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DApSZ99M.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- build/assets/md.transition-legacy-DRitrprI.js
+++ build/assets/md.transition-legacy-DRitrprI.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-FZ1lvydi.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-DApSZ99M.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
