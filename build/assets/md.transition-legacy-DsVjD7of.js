2639a76e04aff51960d6d3cd3da64b548ca4a650
Andrew Barker <andrewbarker96@gmail.com>
2024-05-28 09:24:34 -0500

    Successful Build

--- build/assets/md.transition-legacy-DsVjD7of.js
+++ build/assets/md.transition-legacy-DsVjD7of.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DApSZ99M.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BJRdpeX4.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
