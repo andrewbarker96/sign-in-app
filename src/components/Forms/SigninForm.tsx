909a8045ad6f0e5fea4b964402aad44dd7782f66
Andrew Barker <andrewbarker96@gmail.com>
2024-05-29 12:55:47 -0500

    Error Fix on SigninForm

--- src/components/Forms/SigninForm.tsx
+++ src/components/Forms/SigninForm.tsx
@@ -16,127 +16,124 @@ function SignInForm() {
   const hour = new Date().toLocaleTimeString();
   const date = (new Date().getMonth() + 1) + '-' + (new Date().getDate()) + '-' + new Date().getFullYear();
 
-  const keyboard = Keyboard: {
-    attachFocus: true;
+  const keyboard = Keyboard;
 
-}
-
-const handleFormSubmit = async () => {
-  if (!firstName || !lastName || !email || !company) {
-    setError(true);
-    <IonText color='danger'>All fields must be filled in.</IonText>;
-    return;
-  }
-  else {
-    try {
-      const docRef = await addDoc(collection(firestore, `guests`), {
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        company: company,
-        date: `${date}`,
-        time: `${time}`,
-      });
-      console.log('Document written with ID: ', docRef.id);
-      setSuccess(true);
-      setError(false);
-      setTime(new Date().toLocaleTimeString());
-      setFirstName('');
-      setLastName('');
-      setEmail('');
-      setCompany('');
-
-    } catch (error) {
-      console.error('Error writing document: ', error);
+  const handleFormSubmit = async () => {
+    if (!firstName || !lastName || !email || !company) {
       setError(true);
+      <IonText color='danger'>All fields must be filled in.</IonText>;
+      return;
     }
-  };
-}
+    else {
+      try {
+        const docRef = await addDoc(collection(firestore, `guests`), {
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          company: company,
+          date: `${date}`,
+          time: `${time}`,
+        });
+        console.log('Document written with ID: ', docRef.id);
+        setSuccess(true);
+        setError(false);
+        setTime(new Date().toLocaleTimeString());
+        setFirstName('');
+        setLastName('');
+        setEmail('');
+        setCompany('');
 
-return (
-  <IonContent className='ion-padding'>
-    <IonCardContent>
-      <IonInput
-        id='FirstName'
-        type='text'
-        labelPlacement='floating'
-        placeholder='ex. John'
-        value={firstName}
-        onIonChange={e => setFirstName(e.detail.value || '')}
-      >
-        <IonText slot='label'>
-          First Name
-          <IonText color='danger'>*</IonText>
+      } catch (error) {
+        console.error('Error writing document: ', error);
+        setError(true);
+      }
+    };
+  }
+
+  return (
+    <IonContent className='ion-padding'>
+      <IonCardContent>
+        <IonInput
+          id='FirstName'
+          type='text'
+          labelPlacement='floating'
+          placeholder='ex. John'
+          value={firstName}
+          onIonChange={e => setFirstName(e.detail.value || '')}
+        >
+          <IonText slot='label'>
+            First Name
+            <IonText color='danger'>*</IonText>
+          </IonText>
+        </IonInput>
+      </IonCardContent>
+
+      <IonCardContent>
+        <IonInput
+          id='LastName'
+          type='text'
+          labelPlacement='floating'
+          placeholder='ex. Doe'
+          value={lastName}
+          onIonChange={e => setLastName(e.detail.value || '')}
+        >
+          <IonText slot='label'>
+            Last Name
+            <IonText color='danger'>*</IonText>
+          </IonText>
+        </IonInput>
+      </IonCardContent>
+
+      <IonCardContent>
+        <IonInput
+          id='email'
+          type='email'
+          labelPlacement='floating'
+          placeholder='ex. johndoe@company.com'
+          value={email}
+          onIonChange={e => setEmail(e.detail.value || '')}
+        >
+          <IonText slot='label'>
+            Email
+            <IonText color='danger'>*</IonText>
+          </IonText>
+        </IonInput>
+      </IonCardContent>
+
+      <IonCardContent>
+        <IonInput
+          id='company'
+          type='text'
+          labelPlacement='floating'
+          placeholder='ex. Stock & Associates'
+          value={company}
+          onIonChange={e => setCompany(e.detail.value || '')}
+        >
+          <IonText slot='label'>
+            Company
+            <IonText color='danger'>*</IonText>
+          </IonText>
+        </IonInput>
+      </IonCardContent>
+
+      {error && (
+        <IonText color='danger' className='ion-text-center'>
+          <p>Unable to sign you in. Ensure all fields are filled out.</p>
         </IonText>
-      </IonInput>
-    </IonCardContent>
+      )}
 
-    <IonCardContent>
-      <IonInput
-        id='LastName'
-        type='text'
-        labelPlacement='floating'
-        placeholder='ex. Doe'
-        value={lastName}
-        onIonChange={e => setLastName(e.detail.value || '')}
-      >
-        <IonText slot='label'>
-          Last Name
-          <IonText color='danger'>*</IonText>
+      <IonButton expand='block' onClick={handleFormSubmit}>
+        Sign In
+      </IonButton>
+
+      {success && (
+        <IonText color='success' className='ion-text-center'>
+          You have been successfully signed in!<br />Welcome to Stock & Associates!
         </IonText>
-      </IonInput>
-    </IonCardContent>
 
-    <IonCardContent>
-      <IonInput
-        id='email'
-        type='email'
-        labelPlacement='floating'
-        placeholder='ex. johndoe@company.com'
-        value={email}
-        onIonChange={e => setEmail(e.detail.value || '')}
-      >
-        <IonText slot='label'>
-          Email
-          <IonText color='danger'>*</IonText>
-        </IonText>
-      </IonInput>
-    </IonCardContent>
-
-    <IonCardContent>
-      <IonInput
-        id='company'
-        type='text'
-        labelPlacement='floating'
-        placeholder='ex. Stock & Associates'
-        value={company}
-        onIonChange={e => setCompany(e.detail.value || '')}
-      >
-        <IonText slot='label'>
-          Company
-          <IonText color='danger'>*</IonText>
-        </IonText>
-      </IonInput>
-    </IonCardContent>
-
-    {error && (
-      <IonText color='danger' className='ion-text-center'>
-        <p>Unable to sign you in. Ensure all fields are filled out.</p>
-      </IonText>
-    )}
-
-    <IonButton expand='block' onClick={handleFormSubmit}>
-      Sign In
-    </IonButton>
-
-    {success && (
-      <IonText color='success' className='ion-text-center'>
-        You have been successfully signed in!<br />Welcome to Stock & Associates!
-      </IonText>
-
-    )}
-  </IonContent>
-);
+      )}
+    </IonContent>
+  );
 }
 
 export default SignInForm;
